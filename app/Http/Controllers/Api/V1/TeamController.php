2dd11e35e1280e9e5c9feef51e461de26f97a9ac
Kerem <a.kerem7celik@gmail.com>
2023-06-10 10:14:38 +0300

    api teams endpoint

---
+++ app/Http/Controllers/Api/V1/TeamController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\V1\TeamResource;
+use App\Models\Team;
+use Illuminate\Http\Request;
+
+class TeamController extends Controller
+{
+    public function teams(): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+    {
+        $teams = Team::all()->take(4);
+
+        return TeamResource::collection($teams);
+    }
+}
