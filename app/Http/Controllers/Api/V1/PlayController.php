027ba93fa0e9b973814ec629f1c6be2c748dc4dd
Kerem <a.kerem7celik@gmail.com>
2023-06-11 23:07:36 +0300

    return previous week match results

--- app/Http/Controllers/Api/V1/PlayController.php
+++ app/Http/Controllers/Api/V1/PlayController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\V1\ScoreboardResource;
+use App\Models\Fixture;
 use App\Models\League;
 use App\Services\Play\PlayAllWeeks;
 use App\Services\Play\PlayNextWeek;
@@ -15,23 +16,34 @@ class PlayController extends Controller
     public function playNextWeek(Request $request, League $league)
     {
         (new PlayNextWeek($league))->play();
-        $scoreboardService = new ScoreboardService($league);
 
+        $scoreboardService = new ScoreboardService($league);
         $scoreboardService->scoreboard();
+
         if ($league->fresh()->at_week >= 3) {
             $scoreboardService->championshipPredictions();
         }
 
-        return ScoreboardResource::collection($scoreboardService->returnScoreboard());
+        $previousWeekFixtures = Fixture::query()->where('league_id', $league->id)->where('week', $league->at_week)->get()->toArray();
+
+        return ScoreboardResource::collection($scoreboardService->returnScoreboard())
+            ->additional([
+                'previous_week_fixtures' => $previousWeekFixtures,
+            ]);
     }
 
     public function playAllWeeks(Request $request, League $league)
     {
         (new PlayAllWeeks($league))->play();
-        $scoreboardService = new ScoreboardService($league);
 
+        $scoreboardService = new ScoreboardService($league);
         $scoreboardService->scoreboard();
 
-        return ScoreboardResource::collection($scoreboardService->returnScoreboard());
+        $previousWeekFixtures = Fixture::query()->where('league_id', $league->id)->where('week', $league->fresh()->at_week)->get()->toArray();
+
+        return ScoreboardResource::collection($scoreboardService->returnScoreboard())
+            ->additional([
+                'previous_week_fixtures' => $previousWeekFixtures,
+            ]);
     }
 }
